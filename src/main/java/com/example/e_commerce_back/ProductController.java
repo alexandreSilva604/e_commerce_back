2d93716a9c6f9a0cb6a7a451628320172676c91c
alexandreSilva604 <xandlibra604@gmail.com>
2024-10-02 15:56:52 -0300

    Added ProductController class with allProducts() function, and Spring Web to pom.xml.

---
+++ src/main/java/com/example/e_commerce_back/ProductController.java
@@ -0,0 +1,21 @@
+package com.example.e_commerce_back;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/allProducts")
+    Product[] allProducts() {
+
+        Product[] products = new Product[4];
+
+        products[0] = new Product(1, 10, "World's Best Boss Mug", "Kitchen", "/img/cup.webp");
+        products[1] = new Product(2, 20, "Bedroom Table", "Bedroom", "/img/table.webp");
+        products[2] = new Product(3, 30, "Fridge", "Kitchen", "/img/fridge.jpeg");
+        products[3] = new Product(4, 40, "Kitchen Table", "Kitchen", "/img/table.webp");
+
+        return products;
+    }
+}
